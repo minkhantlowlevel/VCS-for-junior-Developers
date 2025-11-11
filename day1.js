db516827b9f8eac242261d82e9ffa6a6b9dd0cae
minkhantlowlevel <mhein3980@gmail.com>
2025-11-11 13:33:35 +0630

    version1

--- day1.js
+++ day1.js
@@ -1,3 +1,6 @@
 console.log("This is day1");
 console.log("This is testing");
 console.log("This is nothing");
+
+//This is testing for branches
+console.log("Testing branches in versions control system.");
